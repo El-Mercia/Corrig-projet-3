517f391e6c9bfd79c77cf46fcbd4dda634964be0
Mercia <merciarandrianome@gmail.com>
2020-03-23 12:20:03 +0100

    README

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const fs = require("fs");
 
 const app = express();
@@ -11,4 +12,12 @@ app.use(express.static('./public'));
 
 app.get('/get_text', (req, res) => {
     res.send('hello world');
+});
+
+app.get("/api/user/", (req, res) => {
+	console.log ("Get request name");
+    res.send({
+    	name: "Toto",
+
+});
 });
